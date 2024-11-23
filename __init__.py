6728af5f3d462b6e93285e5035b4fef86e73d9a9
Ubuntu <ubuntu@ip-172-31-47-209.ec2.internal>
2024-11-23 06:52:51 +0000

    v0.1.14: Cleaned up repository structure

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from aegis_framework_dev.core.master_agent import MasterAIAgent
-from aegis_framework_dev.core.ollama_model import OllamaLocalModel
-
-__all__ = ["MasterAIAgent", "OllamaLocalModel"]
